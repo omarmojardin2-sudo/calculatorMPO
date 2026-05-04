feceaad5ad9f307db02f31d28d8f9847f079332a
omarmojardin2-sudo <omarmojardin2@gmail.com>
2026-05-04 16:34:55 +0200

    correccion

---
+++ calculatorMPO/src/main/java/com/dam/calculatorMPO/CalculatorMpoApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorMPO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorMpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorMpoApplication.class, args);
+	}
+
+}
---
+++ calculatorMPO/src/test/java/com/dam/calculatorMPO/CalculatorMpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorMPO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorMpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
